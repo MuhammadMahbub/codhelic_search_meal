b08a922cf170c2cce500db142675e0ed88a6e2ff
Muhammad Mahbub <muhammadmahbub07@gmail.com>
2023-10-30 06:40:30 +0600

    setup

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <h3>{{ meals[0] }}</h3>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import store from "../store";
+
+
+const meals = computed(() => store.state.meals)
+
+</script>
